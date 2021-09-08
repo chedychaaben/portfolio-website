5ffd4f5b8a2addcadb65831da7842247f47ce1d4
Chedy Chaaben <chedychaaben@gmail.com>
2021-09-08 14:44:07 +0000

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('', portfolioviews.homepage, name="homepage")
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
