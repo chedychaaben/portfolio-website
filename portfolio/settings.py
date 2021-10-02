50e6ee5f56edb3768070b4e2ae8202ed61e8e526
Chedy Chaaben <chedychaaben@gmail.com>
2021-10-02 17:07:35 +0100

    Adding postgresql 2nd attempt

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
     #}
     'default': {
 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
 
         'NAME': 'dfvbko921smg8a',
 
